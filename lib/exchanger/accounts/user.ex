e16e3523b8e1be96e4b449cb1d525a8d09fe4b07
Alan Vardy <alanvardy@gmail.com>
2020-06-27 07:42:59 -0700

    Add users

---
+++ lib/exchanger/accounts/user.ex
@@ -0,0 +1,18 @@
+defmodule Exchanger.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name])
+    |> validate_required([:first_name, :last_name])
+  end
+end
---
+++ priv/repo/migrations/20200627144107_create_users.exs
@@ -0,0 +1,13 @@
+defmodule Exchanger.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+
+      timestamps()
+    end
+
+  end
+end

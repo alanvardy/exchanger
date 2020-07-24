9eee4f4f58720b77d3e5159fc82f44300d6076e0
Alan Vardy <alanvardy@gmail.com>
2020-07-23 21:24:17 -0700

    Clean up accounts

--- lib/exchanger/accounts.ex
+++ lib/exchanger/accounts.ex
@@ -5,195 +5,62 @@ defmodule Exchanger.Accounts do
 
   import Ecto.Query, warn: false
   alias Exchanger.Repo
+  alias Exchanger.Accounts.{Transaction, User, Wallet}
 
-  alias Exchanger.Accounts.User
+  @type id :: pos_integer
+  @type success_tuple(struct) :: {:ok, struct} | {:error, Ecto.Changeset.t()}
 
-  @doc """
-  Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
-  """
+  @spec list_users :: [User.t()]
   def list_users do
     Repo.all(User)
   end
 
-  @doc """
-  Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
-  """
+  @spec get_user!(id) :: User.t()
   def get_user!(id), do: Repo.get!(User, id)
 
-  @doc """
-  Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec create_user(map) :: success_tuple(User.t())
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec update_user(User.t(), map) :: success_tuple(User.t())
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a user.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec delete_user(User.t()) :: success_tuple(User.t())
   def delete_user(%User{} = user) do
     Repo.delete(user)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
-  """
+  @spec change_user(User.t(), map) :: Ecto.Changeset.t()
+  @spec change_user(User.t()) :: Ecto.Changeset.t()
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
   end
 
-  alias Exchanger.Accounts.Wallet
-
-  @doc """
-  Returns the list of wallets.
-
-  ## Examples
-
-      iex> list_wallets()
-      [%Wallet{}, ...]
-
-  """
   def list_wallets do
     Repo.all(Wallet)
   end
 
-  @doc """
-  Gets a single wallet.
-
-  Raises `Ecto.NoResultsError` if the Wallet does not exist.
-
-  ## Examples
-
-      iex> get_wallet!(123)
-      %Wallet{}
-
-      iex> get_wallet!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_wallet!(id), do: Repo.get!(Wallet, id)
 
-  @doc """
-  Creates a wallet.
-
-  ## Examples
-
-      iex> create_wallet(%{field: value})
-      {:ok, %Wallet{}}
-
-      iex> create_wallet(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_wallet(attrs) do
     attrs
     |> Wallet.create_changeset()
     |> Repo.insert()
   end
 
-  alias Exchanger.Accounts.Transaction
-
-  @doc """
-  Returns the list of transactions.
-
-  ## Examples
-
-      iex> list_transactions()
-      [%Transaction{}, ...]
-
-  """
   def list_transactions do
     Repo.all(Transaction)
   end
 
-  @doc """
-  Gets a single transaction.
-
-  Raises `Ecto.NoResultsError` if the Transaction does not exist.
-
-  ## Examples
-
-      iex> get_transaction!(123)
-      %Transaction{}
-
-      iex> get_transaction!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_transaction!(id), do: Repo.get!(Transaction, id)
 
-  @doc """
-  Creates a transaction.
-
-  ## Examples
-
-      iex> create_transaction(%{field: value})
-      {:ok, %Transaction{}}
-
-      iex> create_transaction(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_transaction(attrs) do
     attrs
     |> Transaction.create_changeset()

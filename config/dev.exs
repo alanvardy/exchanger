0843bcf8bb7af690206a38f49a527a48438e4e00
Alan Vardy <alanvardy@gmail.com>
2020-08-16 07:56:54 -0700

    add docker compose

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,8 @@ config :exchanger, Exchanger.Repo,
   database: "exchanger_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 6000
 
 # For development, we disable any cache and enable
 # debugging and code reloading.

9b3cb249c5080415a9bfea354518b28779d02f14
Alan Vardy <alanvardy@gmail.com>
2020-09-16 09:00:09 -0700

    add additional tests to net worth query

--- test/exchanger_web/schema/queries/balance_test.exs
+++ test/exchanger_web/schema/queries/balance_test.exs
@@ -16,13 +16,38 @@ defmodule ExchangerWeb.Schema.Queries.BalanceTest do
   end
 
   describe "@net_worth" do
-    test "Can get net worth of a user", %{user: user} do
+    test "Can get net worth of a user when no transactions", %{user: user} do
       balance =
         @net_worth_doc
         |> run_schema(%{"user_id" => user.id, "currency" => "USD"})
         |> get_in(["net_worth", "amount"])
 
       assert balance === 0
+    end
+
+    test "Can get net worth of a user", %{user: user} do
+      wallet = insert(:wallet, currency: "USD", user: user)
+      insert(:deposit, to_currency: "USD", to_amount: 10_000, to_wallet: wallet)
+
+      balance =
+        @net_worth_doc
+        |> run_schema(%{"user_id" => user.id, "currency" => "USD"})
+        |> get_in(["net_worth", "amount"])
+
+      assert balance === 10_000
+    end
+
+    test "Can get net worth of a user over multiple currencies", %{user: user} do
+      wallet = insert(:wallet, currency: "USD", user: user)
+      insert(:deposit, to_currency: "USD", to_amount: 10_000, to_wallet: wallet)
+      insert(:deposit, to_currency: "CAD", to_amount: 20_000, to_wallet: wallet)
+
+      balance =
+        @net_worth_doc
+        |> run_schema(%{"user_id" => user.id, "currency" => "CAD"})
+        |> get_in(["net_worth", "amount"])
+
+      assert balance === 40_200
     end
   end
 end
